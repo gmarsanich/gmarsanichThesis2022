d11ad35cc035ee79ce28ee6a8738bdfe683b1f3d
gmarsanich <giacomomarsanich@gmail.com>
2022-04-19 16:07:24 +0200

    Added checkpoints to analysis steps

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import os
 import logging
-from get_video import get_comments, load_comments, get_likes, get_id
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from textblob import TextBlob
-from transformers import pipeline, AutoTokenizer, TFAutoModelForSequenceClassification
+import os
+import time
+
 import pandas as pd
-import spacy
-from spacy_langdetect import LanguageDetector
-from spacy.language import Language
+from textblob import TextBlob
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, pipeline
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+from get_video import get_comments, get_id, get_likes, load_comments
+import utils
 
 # Suppressing warnings and setting up TensorFlow GPU
 logging.disable(logging.WARNING)
@@ -15,47 +16,11 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
-def detect_language(text: str) -> str:
-
-    """This function takes a string and detects the language it's most likely written in using the SpaCy library"""
-
-    def get_lang_detector(nlp, name):
-        """Required by SpaCy"""
-        return LanguageDetector()
-
-    nlp = spacy.load("en_core_web_sm")
-    Language.factory("language_detector", func=get_lang_detector)
-    nlp.add_pipe("language_detector", last=True)
-    doc = nlp(text)
-    return doc._.language["language"]
-
-
-def show_analysis(
-    analyzed_comments: list, save: bool = False, filename: str = None
-) -> None:
-
-    """This function prints the analysis of the given list of comments
-    The function has 2 optional parameters: save and filename. To save the results to a CSV file, save must be True and a filename must be provided.
-    If a filename is not provided and save is True, the function will throw an assertion error.
-    If a filename is provided and save is False, the function will not save the results to file.
-    """
-
-    print(f"Analysis of comments")
-    df = pd.DataFrame(analyzed_comments)
-    df.style.set_properties(**{"text-align": "center"})
-    print(df.head())
-    if save:
-        assert filename is not None, "You must provide a title for the filename"
-        df.to_csv(f"analysis_{filename}.csv")
-        print(f"saved to <analysis_{filename}.csv>")
-        print("\n")
-    return df
-
-
 def main():
-    url = str(input("Enter a YouTube URL: "))
-    comments = load_comments(url)
-    video_id = get_id(url)
+    # url = str(input("Enter a YouTube URL: "))
+    # comments = get_comments(url)
+    comments = load_comments("")
+    # video_id = get_id(url)
 
     ### Analysis
     df_list = []
@@ -73,8 +38,17 @@ def main():
         negative sentiment: compound score <= -0.05
     NOTE: The compound score is the one most commonly used for sentiment analysis by most researchers, including the authors."""
 
-    for comment in comments:
-        lang = detect_language(comment)
+    checkpoints = {
+        int(len(comments) * perc): perc * 100 for perc in [0.25, 0.5, 0.75, 0.9, 1]
+    }
+
+    start_time = time.time()
+
+    for i, comment in enumerate(comments):
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+
+        lang = utils.detect_language(comment)
 
         # Vader
         vs = analyzer.polarity_scores(comment)
@@ -98,10 +72,17 @@ def main():
 
         df_list.append(df_dict)
 
-    show_analysis(df_list, save=True, filename=f"{video_id}")
+        if i + 1 in checkpoints:
+            print(
+                f"{checkpoints[i+1] :.0f}% of the comments have been analyzed\nTime elapsed: {elapsed_time :.2f} seconds"
+            )  # shamelessly stolen from Comp. Ling. Notebook 8
 
-    print(get_likes(url))  # only prints likes and dislikes
-    print(get_likes(url, v=True))  # prints all data related to the video
+    df = utils.show_analysis(df_list, 10)
+    print(len(comments))
+    utils.save_analysis(df, "test")
+
+    # print(get_likes(url))  # only prints likes and dislikes
+    # print(get_likes(url, v=True))  # prints all data related to the video
 
 
 if __name__ == "__main__":
